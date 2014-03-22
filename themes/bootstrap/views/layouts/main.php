101d16c95e0be45798d605706d9cc97ad0672200
hmuni006 <hmuni006@fiu.edu>
2014-03-22 12:28:16 -0400

    ticket in main

--- themes/bootstrap/views/layouts/main.php
+++ themes/bootstrap/views/layouts/main.php
@@ -43,7 +43,7 @@
 					//array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 					//array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
 					array('label'=>'Message', 'url'=>array('/message'), 'visible'=>!Yii::app()->user->isGuest ),
-					array('label'=>'Create Ticket', 'url'=>array('/ticket/create'), 'visible'=>!Yii::app()->user->isGuest )
+					array('label'=>'Ticket', 'url'=>array('/ticket/create'), 'visible'=>!Yii::app()->user->isGuest )
 				)
 			),
 			array(
