8bfe91d603a8c3852d61394c7c74c87be590d586
hmuni006 <hmuni006@fiu.edu>
2014-03-22 14:12:22 -0400

    search

--- protected/views/user/_search.php
+++ protected/views/user/_search.php
@@ -3,6 +3,7 @@
 /* @var $model User */
 /* @var $form CActiveForm */
 ?>
+<link href="../../../bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" />
 
 <div class="wide form">
 
@@ -10,121 +11,87 @@
 	'action'=>Yii::app()->createUrl($this->route),
 	'method'=>'get',
 )); ?>
-
-	<div class="row">
+	<div id="regbox">
 		<?php echo $form->label($model,'id'); ?>
 		<?php echo $form->textField($model,'id',array('size'=>11,'maxlength'=>11)); ?>
-	</div>
-
-	<div class="row">
+	
 		<?php echo $form->label($model,'username'); ?>
 		<?php echo $form->textField($model,'username',array('size'=>45,'maxlength'=>45)); ?>
-	</div>
-
-	<div class="row">
+	
 		<?php echo $form->label($model,'email'); ?>
 		<?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>255)); ?>
-	</div>
-
-	<div class="row">
+	
 		<?php echo $form->label($model,'fname'); ?>
 		<?php echo $form->textField($model,'fname',array('size'=>45,'maxlength'=>45)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'mname'); ?>
-		<?php echo $form->textField($model,'mname',array('size'=>45,'maxlength'=>45)); ?>
-	</div>
-
-	<div class="row">
+	
 		<?php echo $form->label($model,'lname'); ?>
 		<?php echo $form->textField($model,'lname',array('size'=>60,'maxlength'=>100)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'pic_url'); ?>
-		<?php echo $form->textField($model,'pic_url',array('size'=>60,'maxlength'=>255)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'activated'); ?>
-		<?php echo $form->textField($model,'activated'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'activation_chain'); ?>
-		<?php echo $form->textField($model,'activation_chain',array('size'=>45,'maxlength'=>45)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'disable'); ?>
-		<?php echo $form->textField($model,'disable'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'biography'); ?>
-		<?php echo $form->textField($model,'biography',array('size'=>60,'maxlength'=>500)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'linkedin_id'); ?>
-		<?php echo $form->textField($model,'linkedin_id',array('size'=>45,'maxlength'=>45)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'fiucs_id'); ?>
-		<?php echo $form->textField($model,'fiucs_id',array('size'=>45,'maxlength'=>45)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'google_id'); ?>
-		<?php echo $form->textField($model,'google_id',array('size'=>45,'maxlength'=>45)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'isAdmin'); ?>
-		<?php echo $form->textField($model,'isAdmin'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'isProMentor'); ?>
-		<?php echo $form->textField($model,'isProMentor'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'isPerMentor'); ?>
-		<?php echo $form->textField($model,'isPerMentor'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'isDomMentor'); ?>
-		<?php echo $form->textField($model,'isDomMentor'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'isStudent'); ?>
-		<?php echo $form->textField($model,'isStudent'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'isMentee'); ?>
-		<?php echo $form->textField($model,'isMentee'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'isJudge'); ?>
-		<?php echo $form->textField($model,'isJudge'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'isEmployer'); ?>
-		<?php echo $form->textField($model,'isEmployer'); ?>
-	</div>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Search'); ?>
-	</div>
-
-<?php $this->endWidget(); ?>
+	
+		<?php 
+			echo $form->checkBox($model,'disable',array('style'=>'float:left'));
+			echo $form->label($model,'disable'); 
+		?>
+        <?php 
+			echo $form->checkBox($model,'activated',array('style'=>'float:left'));
+			echo $form->label($model,'activated'); 
+		?>
+    </div>
+    <div id="searchbox1" style="margin-left:300px; width:220px!important">
+    		<?php 
+				echo $form->labelEx($model,'men_role');
+				echo $form->checkBox($model,'isProMentor',array('style'=>'float:left'));
+			?>
+			<p style="float:left; margin-left:5px">Project Mentor</p></br></br>
+			<?php 
+				echo $form->checkBox($model,'isPerMentor',array('style'=>'float:left'));
+			?>
+			<p style="float:left; margin-left:5px">Personal Mentor</p></br></br>
+			<?php 
+				echo $form->checkBox($model,'isDomMentor',array('style'=>'float:left'));
+			?>
+			<p style="float:left; margin-left:5px">Domain Mentor</p></br></br>
+			<?php 
+				echo $form->checkBox($model,'isMentee',array('style'=>'float:left'));
+			?>
+			<p style="float:left; margin-left:5px">Mentee</p></br></br>			
+    </div>
+    <div id="searchbox2" style="margin-left:300px; width:220px!important">
+		<?php 
+            echo $form->labelEx($model,'vjf_role');?>
+            <table>
+            	<tr>
+                	<td>
+	                    <?php 
+							echo $form->radioButtonList($model, 'vjf_role', array('', ''),array('onchange' => 'menuTypeChange(this.value);')); 
+						?> 
+					</td>
+                    <td>
+                    	<label>&nbsp;Employer</label></br>
+                        <label>&nbsp;Student</label>
+                    <td>
+                </tr>
+            </table>
+        <?php												
+			echo $form->error($model,'vjf_role'); 
+        ?>			
+    </div>
+    <div id="regbox1" style="margin-left:300px; width:220px!important">
+		<?php 
+            echo $form->labelEx($model,'rmj_role');
+            echo $form->checkBox($model,'isJudge',array('style'=>'float:left'));
+        ?>
+        <p style="float:left; margin-left:5px">Judge</p></br></br>
+        <?php 
+            echo $form->checkBox($model,'isStudent',array('style'=>'float:left'));
+        ?>
+        <p style="float:left; margin-left:5px">Student</p></br></br>			
+    </div>
+	<div style="margin-left:300px">
+   		<?php echo CHtml::submitButton('Search', array("class"=>"btn btn-primary")/*$model->isNewRecord ? 'Create' : 'Save'*/); ?>
+   	</div>
+     	
+    <?php $this->endWidget(); ?>  
+    <div style="clear:both"></div>
+	</br>
 
 </div><!-- search-form -->
