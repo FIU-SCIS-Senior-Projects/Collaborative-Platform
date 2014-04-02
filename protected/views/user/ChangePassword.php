7b663578b3a4df866194e36aaa7e5f0182af50c9
Henry D. Muniz Romero <hmuni006@fiu.edu>
2014-04-02 16:48:07 -0400

    changes to chagepassword page

--- protected/views/user/ChangePassword.php
+++ protected/views/user/ChangePassword.php
@@ -11,30 +11,22 @@
 	'id'=>'user-changePassword-form',
 	'enableAjaxValidation'=>false,
 )); ?>
-	
-	<?php if ($error != '') {?>
-	<p style="color:red;"> <?php echo $error?></p>
-	<?php }?>
-	<div class="row">
-		<?php echo $form->labelEx($user,'Old Password'); ?>
-		<?php echo CHtml::passwordField('User[password]', ''); ?>
-		<?php echo $form->error($model,'username'); ?>
-	</div>
-	
-	<div class="row">
-		<?php echo $form->labelEx($model,'New Password'); ?>
-		<?php echo CHtml::passwordField('User[password1]', ''); ?>
-	</div>
-	
-	<div class="row">
-		<?php echo $form->labelEx($model,'Retype New Password'); ?>
-		<?php echo CHtml::passwordField('User[password2]', ''); ?>
-	</div>	
+	<div id="regbox">
+        <?php if ($error != '') {?>
+        <p style="color:red;"> <?php echo $error?></p>
+        <?php }?>
+        <?php echo $form->labelEx($user,'Old Password'); ?>
+        <?php echo CHtml::passwordField('User[password]', ''); ?>
+        <?php echo $form->error($model,'username'); ?>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Submit', array("class"=>"btn btn-primary")); ?>
-	</div>
+        <?php echo $form->labelEx($model,'New Password'); ?>
+        <?php echo CHtml::passwordField('User[password1]', ''); ?>
 
+        <?php echo $form->labelEx($model,'Retype New Password'); ?>
+        <?php echo CHtml::passwordField('User[password2]', ''); ?>
+        </br>
+        <?php echo CHtml::submitButton('Submit', array("class"=>"btn btn-primary")); ?>
+    </div>
 <?php $this->endWidget(); ?>
 
 </div><!-- form -->
