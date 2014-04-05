2dcfe6d47e739e5a03f6065cb23f493b1cbaf745
hmuni006 <hmuni006@fiu.edu>
2014-04-05 18:17:55 -0400

    uer comment

--- protected/models/User.php
+++ protected/models/User.php
@@ -304,26 +304,30 @@ class User extends CActiveRecord
 
         $link = CHtml::link('Click here', 'http://' . Yii::app()->request->getServerName() . '/coplat/index.php');
 
-        $email= Yii::app()->email;
+        $email_mentor = Yii::app()->email;
+        $email_mentor->to = $ticket_mentor->email;
+        $email_mentor->from = 'Collaborative Platform';
+        $email_mentor->subject = 'Comment added to Ticket #'.$ticket->id;
+        $email_mentor->message = "The user ".$ticket_creator->fname." ".$ticket_creator->lname. " has added a new comment to the Ticket #".$ticket->id.". $link to view the comment.";
 
+        $email_creator = Yii::app()->email;
+        $email_creator->to = $ticket_creator->email;
+        $email_creator->from = 'Collaborative Platform';
+        $email_creator->subject = 'Comment added to Ticket #'.$ticket->id;
+        $email_creator->message = "The Domain Mentor ".$ticket_mentor->fname." ".$ticket_mentor->lname. " has added a new comment to the Ticket #".$ticket->id.". $link to view the comment.";
 
         if($ticket_creator->id == $comment_creator_id)
         {
-            $email->to = $ticket_mentor->email;
-            $email->from = 'Collaborative Platform';
-            $email->subject = 'Comment added to Ticket #'.$ticket->id;
-            $email->message = "The user ".$ticket_creator->fname." ".$ticket_creator->lname. " has added a new comment to the Ticket #".$ticket->id.". $link to view the comment.";
-            $email->send();
+            $email_mentor->send();
         }
-        else//if($ticket_mentor->id == $comment_creator_id)
+        elseif($ticket_mentor->id == $comment_creator_id)
         {
-            $email->to = $ticket_creator->email;
-            $email->from = 'Collaborative Platform';
-            $email->subject = 'Comment added to Ticket #'.$ticket->id;
-            $email->message = "The Domain Mentor ".$ticket_mentor->fname." ".$ticket_mentor->lname. " has added a new comment to the Ticket #".$ticket->id.". $link to view the comment.";
-            $email->send();
+            $email_creator->send();
         }
-
+        else{
+            //$email_creator->send();
+            $email_mentor->send();
+        }
     }
     public static function sendUserNotificationMessageAlert($sender, $reciver, $link, $level)
     {
