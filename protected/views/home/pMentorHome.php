1451cc8caf889c679c36717907fb70c26c81563a
hmuni006 <hmuni006@fiu.edu>
2014-04-05 12:51:34 -0400

    update projectmentor home

--- protected/views/home/pMentorHome.php
+++ protected/views/home/pMentorHome.php
@@ -16,8 +16,6 @@
 
 <!-- Js for popover -->
 <meta charset="UTF-8">
-<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
-<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
 <script type="text/javascript">
     $(document).ready(function () {
         $("a.enable-tooltip").tooltip({
