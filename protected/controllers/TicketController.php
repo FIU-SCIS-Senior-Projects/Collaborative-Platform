2436c1aae13e58e22add6a23beb569659079c516
lsanc104 <lsanc104@fiu.edu>
2014-04-12 18:30:50 -0400

    Updates
    Updastes

--- protected/controllers/TicketController.php
+++ protected/controllers/TicketController.php
@@ -28,15 +28,15 @@ class TicketController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-                'actions' => array('index', 'view',  'adminHome'),
+                'actions' => array('index', 'view',  'adminHome','userHome'),
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-                'actions' => array('create', 'update', 'Download'),
+                'actions' => array('create', 'update', 'Download','adminHome','userHome'),
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
+				'actions'=>array('admin','delete','adminHome','userHome'),
 				'users'=>array('admin'),
 			),
 			array('deny',  // deny all users
