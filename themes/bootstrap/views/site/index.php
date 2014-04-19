ea722a07702808df8d69cae0255883bda8122f7d
hmuni006 <hmuni006@fiu.edu>
2014-04-19 12:52:42 -0400

    fix to index

--- themes/bootstrap/views/site/index.php
+++ themes/bootstrap/views/site/index.php
@@ -1,17 +1,14 @@
 <?php
 	if(Yii::app()->user->isGuest)
 	    $this->redirect("/coplat/index.php/site/login");
-<<<<<<< HEAD
 
-
-=======
     elseif(User::isCurrentUserAdmin())
         $this->redirect("/coplat/index.php/home/adminHome");
     else
         $this->redirect("/coplat/index.php/home/userHome");
 	
 	
->>>>>>> FETCH_HEAD
+
 	//$this->pageTitle=Yii::app()->name;
 	$this->beginWidget('bootstrap.widgets.TbHeroUnit',array(
     'heading'=>CHtml::encode(Yii::app()->name
