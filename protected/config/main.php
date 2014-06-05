b73e85e1587565e82e8f5b067883e3efe8ed2c5a
rgome020 <rgome020@fiu.edu>
2014-06-05 19:04:19 -0400

    Update 2
    Update2

--- protected/config/main.php
+++ protected/config/main.php
@@ -71,7 +71,7 @@ return array(
 			'connectionString' => 'mysql:host=localhost;dbname=coplat',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => 'root',
+			'password' => '',
 			'charset' => 'utf8',
 		),
 		
