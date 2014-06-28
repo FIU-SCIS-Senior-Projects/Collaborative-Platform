06efadc733ce80b44b120353dbb6d91ae6dd37de
rgome020 <rgome020@fiu.edu>
2014-06-28 12:22:34 -0400

    priority3
    priority3

--- protected/views/priority/admin.php
+++ protected/views/priority/admin.php
@@ -9,7 +9,7 @@ $this->breadcrumbs=array(
 
 $this->menu=array(
 	array('label'=>'List Priority', 'url'=>array('index')),
-	array('label'=>'Create Priority', 'url'=>array('create')),
+	//array('label'=>'Create Priority', 'url'=>array('create')),
 );
 
 Yii::app()->clientScript->registerScript('search', "
@@ -28,10 +28,6 @@ $('.search-form form').submit(function(){
 
 <h1>Manage Priorities</h1>
 
-<p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
-</p>
 
 <?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
 <div class="search-form" style="display:none">
@@ -50,6 +46,7 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		'reassignHours',
 		array(
 			'class'=>'CButtonColumn',
+            'template'=>'{view}{update}',
 		),
 	),
 )); ?>
