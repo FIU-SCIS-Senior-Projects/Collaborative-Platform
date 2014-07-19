3021a014c6cca1f35c16d9565242f1db137b9218
jsant096 <jsant096@fiu.edu>
2014-07-19 10:09:37 -0400

    testing 31

--- protected/views/profile/userProfile.php
+++ protected/views/profile/userProfile.php
@@ -312,7 +312,8 @@
                            foreach($userdom as $udom )
                            {
                                if($udom->subdomain_id!=null)
-                               {                               $subdm = Subdomain::model()->findBySql("select * from subdomain where id = $udom->subdomain_id");
+                               {
+                                   $subdm = Subdomain::model()->findBySql("select * from subdomain where id = $udom->subdomain_id");
                                $res =  $subdm->name.' / '.$udom->rate.' / '.$udom->tier_team.'<br>';
                                }
                            }
