1eafb576e2de0ee7905498c4de864a8f8b69033f
Jonathan Sanchez <sanchez.jonathan@gmail.com>
2014-09-28 16:10:56 -0400

    chmod changes for mac and MVC for applications

---
+++ protected/controllers/ApplicationController.php
@@ -0,0 +1,56 @@
+<?php
+
+class ApplicationController extends Controller
+{
+	public function actionCreate()
+	{
+		$this->render('create');
+	}
+
+	public function actionDelete()
+	{
+		$this->render('delete');
+	}
+
+	public function actionImport()
+	{
+		$this->render('import');
+	}
+
+	public function actionUpdate()
+	{
+		$this->render('update');
+	}
+
+	public function actionView()
+	{
+		$this->render('view');
+	}
+
+	// Uncomment the following methods and override them if needed
+	/*
+	public function filters()
+	{
+		// return the filter configuration for this controller, e.g.:
+		return array(
+			'inlineFilterName',
+			array(
+				'class'=>'path.to.FilterClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+
+	public function actions()
+	{
+		// return external action classes, e.g.:
+		return array(
+			'action1'=>'path.to.ActionClass',
+			'action2'=>array(
+				'class'=>'path.to.AnotherActionClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+	*/
+}
