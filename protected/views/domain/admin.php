b6ec0dbeb879c10a0d70acd3166c24d6fda7a8c1
Jonathan Sanchez <Sanchez.jonathan@gmail.com>
2014-10-17 21:40:45 -0400

    for all pages fixed column and uses. update all views to display modal. next to display data effectively.

--- protected/views/domain/admin.php
+++ protected/views/domain/admin.php
@@ -22,7 +22,53 @@ $this->breadcrumbs=array(
 		'need',
 		'need_amount',
 		array(
-			'class'=>'CButtonColumn',
-		),
+    				'header'=>'Options',
+    				'class'=>'bootstrap.widgets.TbButtonColumn',
+    				'template'=> '{view} {delete}',
+    				'buttons'=>array(
+    						'view'=>
+    						array(
+    								'url'=>'Yii::app()->createUrl("domain/view", array("id"=>$data->id))',
+    								'options'=>array(
+    										'ajax'=>array(
+    												'type'=>'POST',
+    												'url'=>"js:$(this).attr('href')",
+    												'success'=>'function(data) { $("#viewModal .modal-body p").html(data); $("#viewModal").modal(); }'
+    										),
+    								),
+    						),
+    				),
+    		) 
 	),
 )); ?>
+
+
+<!-- View Popup  -->
+<?php $this->beginWidget('bootstrap.widgets.TbModal', array('id'=>'viewModal')); ?>
+<!-- Popup Header -->
+
+<div class="modal-header">
+    <h4>View Employee Details</h4>
+
+</div>
+
+<!-- Popup Content -->
+<div class="modal-body">
+    <p>Employee Details</p>
+
+</div>
+<!-- Popup Footer -->
+<div class="modal-footer">
+
+    <!-- close button -->
+    <?php $this->widget('bootstrap.widgets.TbButton', array(
+        'label'=>'Close',
+        'url'=>'#',
+        'htmlOptions'=>array('data-dismiss'=>'modal'),
+    )); ?>
+    <!-- close button ends-->
+</div>
+<?php $this->endWidget(); ?>
+<!-- View Popup ends -->
+
+
--- protected/views/project/admin.php
+++ protected/views/project/admin.php
@@ -44,9 +44,52 @@ $('.search-form form').submit(function(){
 		'start_date',
 		'due_date',
 		array(
-			'header'=>'Options',
-			'class'=>'bootstrap.widgets.TbButtonColumn',
-				'template'=>'{view}'
-		),
+    				'header'=>'Options',
+    				'class'=>'bootstrap.widgets.TbButtonColumn',
+    				'template'=> '{view}',
+    				'buttons'=>array(
+    						'view'=>
+    						array(
+    								'url'=>'Yii::app()->createUrl("project/view", array("id"=>$data->id))',
+    								'options'=>array(
+    										'ajax'=>array(
+    												'type'=>'POST',
+    												'url'=>"js:$(this).attr('href')",
+    												'success'=>'function(data) { $("#viewModal .modal-body p").html(data); $("#viewModal").modal(); }'
+    										),
+    								),
+    						),
+    				),
+    		) 
 	),
 )); ?>
+
+
+<!-- View Popup  -->
+<?php $this->beginWidget('bootstrap.widgets.TbModal', array('id'=>'viewModal')); ?>
+<!-- Popup Header -->
+
+<div class="modal-header">
+    <h4>View Employee Details</h4>
+
+</div>
+
+<!-- Popup Content -->
+<div class="modal-body">
+    <p>Employee Details</p>
+
+</div>
+<!-- Popup Footer -->
+<div class="modal-footer">
+
+    <!-- close button -->
+    <?php $this->widget('bootstrap.widgets.TbButton', array(
+        'label'=>'Close',
+        'url'=>'#',
+        'htmlOptions'=>array('data-dismiss'=>'modal'),
+    )); ?>
+    <!-- close button ends-->
+</div>
+<?php $this->endWidget(); ?>
+<!-- View Popup ends -->
+
