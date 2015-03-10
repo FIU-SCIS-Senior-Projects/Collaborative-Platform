d22c6d80872b8acb5095bf0a3918a261821e770a
Jorge Travieso <jtrav029@fiu.edu>
2015-03-10 12:47:13 -0400

    organized the sql script for the video conferences and hard coded the invitee name on the email.

--- Code/WebSite/coplat/protected/controllers/VideoConferenceController.php
+++ Code/WebSite/coplat/protected/controllers/VideoConferenceController.php
@@ -102,7 +102,8 @@ class VideoConferenceController extends Controller
                     if (!$invitation->save()) {                                         //an error occurred
                         $invitationError .= "An error occurred upon saving the invitation to " . $email . "error";
                     } else {
-                        $moderatorfullName = Yii::app()->user->getFullName();           //this current user
+                        //$moderatorfullName = Yii::app()->user->getFullName();           //this current user
+                        $moderatorfullName = "Name Here"
                         $inviteefullName = $invitee->fname . " " . $invitee->lname;
 
                         VCInvitation::sendInvitationEmail($model->id, $moderatorfullName, $inviteefullName, $email);;
