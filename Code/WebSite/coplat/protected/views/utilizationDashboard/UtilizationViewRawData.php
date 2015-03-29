e594649168f2cef73572d45cf104d341f620177f
aalfo077@fiu.edu <aalfo077@fiu.edu>
2015-03-29 12:10:14 -0400

    Implemented the tiecket unanswered raw report

--- Code/WebSite/coplat/protected/views/utilizationDashboard/UtilizationViewRawData.php
+++ Code/WebSite/coplat/protected/views/utilizationDashboard/UtilizationViewRawData.php
@@ -24,11 +24,17 @@
                break;  
 
             case ReportType::TicketsCurrentlyOpen:
+			     
 			 $columns[] =  array('name'  => 'OpenedSince',
                                 'header'=> 'Opened Since (hours)',
                                 'headerHtmlOptions' => array('width'=>'120', ));       
                break;  
-
+			   
+			case ReportType::TicketsUnanswered:
+             $columns[] =  array('name'  => 'OpenedSince',
+                                'header'=> 'Opened Since (hours)',
+                                'headerHtmlOptions' => array('width'=>'120', ));       
+               break;  
 			
 
 			   
