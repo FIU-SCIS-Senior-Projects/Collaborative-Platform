bb3b47261457ea25eb17189c0b8613428717f794
Mandiel Lastra <Mandy@mandiels-mbp.attlocal.net>
2015-05-27 23:24:26 -0400

    troubleshooting webcam on/off feature

--- Code/WebSite/coplat/protected/views/videoConference/join.php
+++ Code/WebSite/coplat/protected/views/videoConference/join.php
@@ -164,7 +164,7 @@
         <div id="video-container" style="" class="col-md-2 col-lg-3">
 
             <div class="col-md-offset-5">
-                <?php echo '<i onclick="pauseResumeVideo()" class="fa fa-video-camera" id="on-off-video"></i>'?>
+                <?php echo '<i class="fa fa-video-camera" id="on-off-video"></i>'?>
             </div>
 
         </div>
@@ -277,12 +277,14 @@
                 audio: true,
                 video: true
             });
+            document.getElementById("on-off-video").style.color= 'red';
             video_status = 0;
         } else if (video_status == 0) {
             rmc.mute({
                 audio: true,
                 video: false
             });
+            document.getElementById("on-off-video").style.color= 'gray';
             video_status = 1;
         }
     });
