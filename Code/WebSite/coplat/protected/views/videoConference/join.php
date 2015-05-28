612a0ea117a3ca1e3149aef317af3f121c1cbd93
Mandiel Lastra <Mandy@mandiels-mbp.attlocal.net>
2015-05-28 18:08:58 -0400

    troubleshooting webcam on/off feature

--- Code/WebSite/coplat/protected/views/videoConference/join.php
+++ Code/WebSite/coplat/protected/views/videoConference/join.php
@@ -272,7 +272,7 @@
     var video_status = 0;
 
     function pauseResumeVideo() {
-        rmc.streams['rmc'].mute({'video' : true});
+        connection.streams.mute();
         // if(video_status == 0) {
         //     document.getElementById("on-off-video").style.color= 'red';
         //     rmc.removeStream({'video' : true});
