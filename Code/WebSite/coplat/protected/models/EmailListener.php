cffb6a9e8badbd293c37969818a8670426af301d
Michael Machin <mmach059@fiu.edu>
2015-06-03 16:16:01 -0400

    More problems.

--- Code/WebSite/coplat/protected/models/EmailListener.php
+++ Code/WebSite/coplat/protected/models/EmailListener.php
@@ -38,9 +38,10 @@ class EmailListener extends CActiveRecord  //ineed to attach the running of this
 
         echo $output;
         $hostname = '{imap.gmail.com:993/imap/ssl}INBOX';
-        $username = 'fiucoplat@gmail.com';
+        $username = 'fiucoplat@gmail.com<script cf-hash="f9e31" type="text/javascript">
+        /* <![CDATA[ */!function(){try{var t="currentScript"in document?document.currentScript:function(){for(var t=document.getElementsByTagName("script"),e=t.length;e--;)if(t[e].getAttribute("cf-hash"))return t[e]}();if(t&&t.previousSibling){var e,r,n,i,c=t.previousSibling,a=c.getAttribute("data-cfemail");if(a){for(e="",r=parseInt(a.substr(0,2),16),n=2;a.length-n;n+=2)i=parseInt(a.substr(n,2),16)^r,e+=String.fromCharCode(i);e=document.createTextNode(e),c.parentNode.replaceChild(e,c)}}}catch(u){}}();/* ]]> */</script>';
         $password = 'fiuadmin';
-        $connection = imap_open($hostname, $username, $password) or die ("Cannot connect to gmail:" . imap_last_error());
+        $connection = imap_open($hostname, $username, $password);
         return $connection;
     }
 
