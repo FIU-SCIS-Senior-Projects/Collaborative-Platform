4cd91ac3e0df104d9f4cb83f459b5c3b72390b9f
Mandiel Lastra <Mandy@mandiels-mbp.attlocal.net>
2015-06-04 00:46:37 -0400

    add second screen area

--- Code/WebSite/coplat/protected/views/videoConference/join.php
+++ Code/WebSite/coplat/protected/views/videoConference/join.php
@@ -158,13 +158,13 @@
 
 <div class="container-fluid">
     <div class="row-fluid">
-        <div class="span2" style="width: 100%">
+        <!-- <div class="span2" style="width: 2"> -->
             <div id="video-container" style="" class="col-md-2 col-lg-2">
 
                 <div class="col-md-offset-6 col-lg-offset-7">
                     <?php echo '<i onclick="pauseResumeVideo()" class="fa fa-video-camera" style="color: #FFF" id="on-off-video"></i>'?></div>
             </div>
-        </div>
+        <!-- </div> -->
         <div class="span10">
             <!-- <div class="row"> -->
                 <div id="cotools-container" class="col-md-5 col-lg-5">
