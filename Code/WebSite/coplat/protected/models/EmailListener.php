03407e826e488b67cbbbc40c1749242e9d2b8f1e
Michael Machin <mmach059@fiu.edu>
2015-06-05 07:48:25 -0700

    complete rewrite first test

--- Code/WebSite/coplat/protected/models/EmailListener.php
+++ Code/WebSite/coplat/protected/models/EmailListener.php
@@ -5,139 +5,132 @@
  * Date: 5/25/2015
  * Time: 5:33 PM
  */
-class EmailListener extends CActiveRecord  //ineed to attach the running of this class to some thing to make sure it does, cause it doo
-{
-    public static function model($className=__CLASS__)
-    {
-        return parent::model($className);
-    }
-    public function tableName()
-    {
-        return 'email_listener';
-    }
-    public function rules()
-    {
-        // NOTE: you should only define rules for those attributes that
-        // will receive user inputs.
-        return array(
-            array('pid', 'required'),
-            array('pid', 'integerOnly'=>true),
-            array('userID', 'safe', 'on'=>'search'),
-        );
-    }
-    public function attributeLabels()
-    {
-        return array(
-            'pid' => 'Process ID',
 
-        );
-    }
-    public function establishConnection()
+    function establishConnection()
     {
-        $output = "<script>console.log( 'it went in here' );</script>";
-
-        echo $output;
         $hostname = '{imap.gmail.com:993/imap/ssl}INBOX';
-        $username = 'fiucoplat@gmail.com<script cf-hash="f9e31" type="text/javascript">
-        /* <![CDATA[ */!function(){try{var t="currentScript"in document?document.currentScript:function(){for(var t=document.getElementsByTagName("script"),e=t.length;e--;)if(t[e].getAttribute("cf-hash"))return t[e]}();if(t&&t.previousSibling){var e,r,n,i,c=t.previousSibling,a=c.getAttribute("data-cfemail");if(a){for(e="",r=parseInt(a.substr(0,2),16),n=2;a.length-n;n+=2)i=parseInt(a.substr(n,2),16)^r,e+=String.fromCharCode(i);e=document.createTextNode(e),c.parentNode.replaceChild(e,c)}}}catch(u){}}();/* ]]> */</script>';
+        $username = 'fiucoplat@gmail.com';//<script cf-hash="f9e31" type="text/javascript">
+        ///* <![CDATA[ */!function(){try{var t="currentScript"in document?document.currentScript:function(){for(var t=document.getElementsByTagName("script"),e=t.length;e--;)if(t[e].getAttribute("cf-hash"))return t[e]}();if(t&&t.previousSibling){var e,r,n,i,c=t.previousSibling,a=c.getAttribute("data-cfemail");if(a){for(e="",r=parseInt(a.substr(0,2),16),n=2;a.length-n;n+=2)i=parseInt(a.substr(n,2),16)^r,e+=String.fromCharCode(i);e=document.createTextNode(e),c.parentNode.replaceChild(e,c)}}}catch(u){}}();/* ]]> */</script>';
         $password = 'fiuadmin';
         $connection = imap_open($hostname, $username, $password);
         return $connection;
     }
-
-    public function emailListener()
+    function establishDBConnection()
     {
-        $output = "<script>console.log( 'just got in' );</script>";
-
-        echo $output;
-        $connection = EmailListener::establishConnection();
-        $output = "<script>console.log( 'set up connection' );</script>";
-
-        echo $output;//develop thread/loop
-        $awayment = new AwayMentor();
-        $messagestatus = "UNSEEN";
-        $countTo24 = 0;
-        //     while (true) {
-        $emails = imap_search($connection, $messagestatus);
-        $output = "<script>console.log( 'did imap search' );</script>";
-
-        echo $output;
-        if ($emails) {
-            rsort($emails);
-            foreach ($emails as $email_number) {
-                $output = "<script>console.log( 'in loop of emails' );</script>";
-
-                echo $output;
-                $header = imap_fetch_overview($connection, $email_number, 0);
-                $message = imap_fetchbody($connection, $email_number, 1.1);
-                if ($message == "") {
-                    $message = imap_fetchbody($connection, $email_number, 1);
-                }
-                if (!$awayment->detectOOOmessage($header->subject, $message, $header->from)) {
-                    $awayment->detectB00message($header->subject, $header->from);
-                }
-                imap_delete($connection, 1); //this might bug out but should delete the top message that was just parsed
-            }
-            //          sleep(600); //do check every 10 minutes
-            $countTo24 = $countTo24 + 1;
-            if ($countTo24 >= 144) {
-                $countTo24 = 0;
-                $command = Yii::app()->db->createCommand();
-                $command->delete('away_mentor', 'tiStamp <= DATE_ADD(CURRENT_DATE , INTERVAL -1 DAY )');//this might bug the hell out deletes mentors on the away list that were put on over 24 hours ago
-            }
-            if (!imap_ping($connection)) {
-                $connection = establishConnection();
-            }
-        }
-    //}
+        $username = 'root';
+        $password = '9Qst32+';
+        $dbconn = new mysqli($username,$password,"coplat");
+        return $dbconn;
     }
 
-   // public function setupKids()
-   // {
-     //   $pid1 = pcntl_fork();
-     //   if($pid1)
-    //    {
-   //         return 1;//success and go back to what you were doing;
-    //    }
-    //    if($pid1==-1)
-   ///     {
-    //        return 0;//didnt work;
-   //     }
-   //     $pid1 = pcntl_fork();
-    //    if($pid1)
-   //    {
-   //         if($pid1 >0) {
-    //            $command = Yii::app()->db->createCommand();
-   //            $command->insert('email_listener', array("pid" => $pid1));//writes to the database letting it know that there SHOULD be a email listener running
-//
-   //             pcntl_waitpid($pid1, $kidStatus);//will just wait untill child dies
-   //             $command = Yii::app()->db->createCommand();
-   //             $command->delete('email_listener', 'pid=:pid1', array('pid1' => $pid1));//removes the child pid from the database letting it know that there DEFINETELY ISNT a child process running;
-   //         }
-   //         if($pid1 = -1)
-   //         {
-   //             exit();
-   //         }
-   //     }
-//          else{
-    //        $output = "<script>console.log( 'In grandchild' );</script>";
-//
-  //          echo $output;
-    //        exit();
-          //  $stat = EmailListener::emailListener();
-      //  }
-   // }
-   // public function getStatus()
-  //  {
-  //      $am = AwayMentor::model()->findAllBySql("SELECT * FROM email_listener");
- //       if ($am)
-  //      {
- //           return 1;
-  //      }
-  //      else{
- //           $stat = EmailListener::setupKids();
-//            return $stat;
- //       }
-//    }
+    function emailListener()
+    {
+        //$output = "<script>console.log( 'just got in' );</script>";
+
+        //echo $output;
+        $connection = establishConnection();
+        $dbConn = establishDBConnection();
+        //$output = "<script>console.log( 'set up connection' );</script>";
+
+        //echo $output;//develop thread/loop
+        $messagestatus = "UNSEEN";
+        $countTo24 = 0;
+        while (true) {
+            $emails = imap_search($connection, $messagestatus);
+            if ($emails) {
+                rsort($emails);
+                foreach ($emails as $email_number) {
+                    $output = "<script>console.log( 'in loop of emails' );</script>";
+
+                    echo $output;
+                    $header = imap_fetch_overview($connection, $email_number, 0);
+                    $message = imap_fetchbody($connection, $email_number, 1.1);
+                    if ($message == "") {
+                        $message = imap_fetchbody($connection, $email_number, 1);
+                    }
+                    if (!detectOOOmessage($header->subject, $message, $header->from, $dbConn)) {
+                       detectB00message($header->subject, $header->from, $dbConn);
+                    }
+                    imap_delete($connection, 1); //this might bug out but should delete the top message that was just parsed
+                }
+                sleep(600); //do check every 10 minutes
+                $countTo24 = $countTo24 + 1;
+                if ($countTo24 >= 144) {
+                    $countTo24 = 0;
+                    $dbConn->query("DELETE FROM away_mentor WHERE tiStamp <= DATE_ADD(CURRENT_DATE, INTERVAL -1 DAY)");//delete mentors that have been away for more than 24 hours from the away list
+                    //$command = Yii::app()->db->createCommand();
+                 //   $command->delete('away_mentor', 'tiStamp <= DATE_ADD(CURRENT_DATE , INTERVAL -1 DAY )');//this might bug the hell out deletes mentors on the away list that were put on over 24 hours ago
+                }
+                if (!imap_ping($connection)) {
+                    $connection = establishConnection();
+                }
+            }
+        }
+    }
+    function detectOOOmessage($subjectline, $body, $email, $dbconnect)
+    {
+
+    if (stristr($subjectline, "Auto") || stristr($subjectline, "out of office"))
+    {
+        if(stristr($body, "out of office"))
+        {
+            $isAwayAlready = $dbconnect->query("SELECT * FROM user  INNER JOIN away_mentor ON user.id = away_mentor.userID WHERE email LIKE '$email'");
+            if(!$isAwayAlready)
+            {
+
+                $awayment = $dbconnect->query("SELECT * FROM user WHERE email LIKE '$email'");
+                //$awayment = User::model()->findAllByAttributes(array('email' => $email));
+                setAsAway($awayment["id"], $dbconnect);
+                return 1;//success
+            }
+            return 0;//is
+        }
+    }
+    return 0;
+    }
+    function detectB00message($subjectline, $email, $dbconnect)
+    {
+    if(stristr($subjectline, "Back in Office"))
+    {
+        $awayment = $dbconnect->query("SELECT * FROM user WHERE email LIKE '$email'");
+        $dbconnect->query("DELETE FROM away_mentor WHERE userID =".$awayment["id"]."limit 1");
+
+    }
+    }
+    function setAsAway($user_Id, $dbconnect)
+    {
+       $dbconnect->query("INSERT INTO away_mentor (userID, tiStamp) VALUES ($user_Id, NOW())");
+
+    $ticketSubs = "";
+    $ftickets = $dbconnect->query("SELECT * FROM ticket WHERE assign_user_id = $user_Id AND assigned_date >= DATE_ADD(CURRENT_DATE , INTERVAL -1 DAY )");//find tickets assigned to this user within last 24 hours
+    while ($aticket = mysql_fetch_assoc($ftickets)) {
+        //reassign the tickets
+        if (!is_null($aticket["subdomain_id"])) {
+            $possibleMentors = $dbconnect->query("SELECT * FROM user_domain WHERE domain_id = " . $aticket["domain_id"] . " AND subdomain_id = " . $aticket["subdomain_id"] . "AND tier_team = 1 AND user_id not in (select userID as user_id from away_mentor) ");
+
+        } else {
+            $possibleMentors = $dbconnect->query("SELECT * FROM user_domain WHERE domain_id = " . $aticket["domain_id"] . " AND tier_team = 1 AND user_id not in (select userID as user_id from away_mentor) ");
+
+        }
+        while ($aMentor = mysql_fetch_assoc($possibleMentors)) {
+            $count = $dbconnect->query("SELECT COUNT(id) as `id` FROM ticket WHERE assign_user_id = " . $aMentor["user_id"]);
+            $adomainMentor = $dbconnect->query("SELECT * FROM domain_mentor WHERE user_id = " . $aMentor["user_id"]);
+            if ($adomainMentor) {
+                if ($count['id'] < $adomainMentor["max_tickets"]) {
+                    $dbconnect->query("UPDATE ticket SET assign_user_id = " . $aMentor["user_domain"] . " WHERE id = " . $aticket["id"]);
+                    //send reassingment email to new mentor
+                }
+            }
+        }
+        $ticketSubs = $ticketSubs . $aticket["subject"] . ", ";
+
+        // do this outside the loop  $awayMent = User::model()->findAllBySql("SELECT * FROM user WHERE id =:user_Id", array(":user_id"=>$user_Id));
+        // foreach ($awayMent as $bawayMent) {
+        //    User::model()->sendEmailTicketCancelOutOfOffice($bawayMent->fname . " " . $bawayMent - lname, $bawayMent->email, $aticket->subject);
+        //}
+    }
+        //email old mentor list of tickets reassigned
+
+
 }
 
