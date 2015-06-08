e97bbcbe663c29495801ca02b4316ce3a0d4bc59
Michael Machin <mmach059@fiu.edu>
2015-06-08 08:53:37 -0700

    put in echo to track progress

--- Code/WebSite/coplat/protected/models/EmailListener.php
+++ Code/WebSite/coplat/protected/models/EmailListener.php
@@ -204,6 +204,7 @@ function checkPriorityElapseTickets()
     // reassign tickets
     if($ticketr->num_rows>0) {
         while ($aticket = $ticketr->fetch_assoc()) {
+            echo "a ticket was found and is going to be reassigned";
             $mentor = $dbconnect->query("Select * from user WHERE id = ".$aticket["assign_user_id"]);
             $aMentor = $mentor->fetch_assoc();
             sendTicketCancelOutOfTime($aMentor["email"], $aticket["subject"]);
@@ -244,6 +245,7 @@ function checkPriorityElapseTickets()
                     }
                 }
             }
+            echo "a went through entire thing ticket should be reassigned";
         }
     }
 }
