f3100324ade3cb729dd8a7945aa67c0107a541e4
Mandiel Lastra <Mandy@mandiels-mbp.attlocal.net>
2015-06-09 22:04:33 -0400

    rearrange webcams

--- Code/WebSite/coplat/protected/views/videoConference/join.php
+++ Code/WebSite/coplat/protected/views/videoConference/join.php
@@ -171,7 +171,7 @@
 
     <div class="row">
 
-        <div id="video-container" style="" class="col-md-2 col-lg-3">
+        <div id="video-container" style="" class="col-md-2 col-lg-2">
 
             <div class="col-md-offset-6 col-lg-offset-7">
                 <?php echo '<i onclick="pauseResumeVideo()" class="fa fa-video-camera" style="color: #FFF" id="on-off-video"></i>'?>
