1d6614ad8e039a8a30ae649891aabe303c1fbeb7
Michael Machin <mmach059@fiu.edu>
2015-06-10 08:20:46 -0700

    testing email trying to correct the envelope

---
+++ Code/WebSite/coplat/testemail.php
@@ -0,0 +1,10 @@
+<?php
+$to      = 'adurocruor@gmail.com';
+$subject = 'a test email';
+$message = 'hello';
+$headers = 'From: fiucoplat@gmail.com' . "\r\n" .
+    'Reply-To: fiucoplat@gmail.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+?>
