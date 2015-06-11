fe76b158833b61a79dbf528eaffa52b96b8a6a26
Michael Machin <mmach059@fiu.edu>
2015-06-11 15:32:04 -0700

    testing something else

--- Code/WebSite/coplat/testemail.php
+++ Code/WebSite/coplat/testemail.php
@@ -5,6 +5,6 @@ $message = 'hello';
 $headers = 'From: Collaborative Platform <fiucoplat@gmail.com>' . "\r\n" .
     'Reply-To: fiucoplat@gmail.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
-
+echo $to;
 mail($to, $subject, $message, $headers);
 ?>
