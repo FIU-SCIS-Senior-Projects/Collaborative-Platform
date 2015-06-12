e7adfafe736fbdf6dce5eadc062493d2170f48e4
Mandiel Lastra <Mandy@Mandiels-MacBook-Pro.local>
2015-06-12 09:36:59 -0400

    second screen share

--- Code/WebSite/coplat/protected/views/videoConference/join.php
+++ Code/WebSite/coplat/protected/views/videoConference/join.php
@@ -386,20 +386,6 @@
 
 
     $('#share-screen-2').click(function () {
-
-        sourceId = null; // remove old capture screen
-        getScreenConstraints(function(error, screen_constraints) {
-            // invoke navigator.getUserMedia here
-            navigator.getUserMedia = navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
-            navigator.getUserMedia(screen_constraints, function (sec) {
-                document.querySelector('#cotools-panel-2 video').src = URL.createObjectURL(sec);
-//                document.getElementById('video').appendChild(this.mediaElement);
-            }, function (error) {
-                console.error(error);
-            });
-        });
-
-        //right = 1;
 //        sec.addStream({
 //            screen: true,
 //            oneway: true
@@ -408,17 +394,50 @@
 //
 //        // http://www.rtcmulticonnection.org/docs/addStream/
 //        getScreenId(function (error, sourceId, screen_constraints) {
-//            // error    == null || 'permission-denied' || 'not-installed' || 'installed-disabled' || 'not-chrome'
-//            // sourceId == null || 'string' || 'firefox'
-//
 //            navigator.getUserMedia = navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
 //            navigator.getUserMedia(screen_constraints, function (sec) {
 //                document.querySelector('#cotools-panel-2 video').src = URL.createObjectURL(sec);
-////                document.getElementById('video').appendChild(this.mediaElement);
 //            }, function (error) {
 //                console.error(error);
 //            });
 //        });
+
+
+        getScreenId(function (error, sourceId, screen_constraints) {
+            // error    == null || 'permission-denied' || 'not-installed' || 'installed-disabled' || 'not-chrome'
+            // sourceId == null || 'string' || 'firefox'
+
+            if(sourceId && sourceId != 'firefox') {
+                screen_constraints = {
+                    video: {
+                        mandatory: {
+                            chromeMediaSource: 'screen',
+                            maxWidth: 1920,
+                            maxHeight: 1080,
+                            minAspectRatio: 1.77
+                        }
+                    }
+                };
+
+                if (error === 'permission-denied') return alert('Permission is denied.');
+                if (error === 'not-chrome') return alert('Please use chrome.');
+
+                if (!error && sourceId) {
+                    screen_constraints.video.mandatory.chromeMediaSource = 'desktop';
+                    screen_constraints.video.mandatory.chromeMediaSourceId = sourceId;
+                }
+            }
+
+            navigator.getUserMedia = navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+            navigator.getUserMedia(screen_constraints, function (stream) {
+                document.querySelector('video').src = URL.createObjectURL(stream);
+            }, function (error) {
+                console.error(error);
+            });
+        });
+
+
+
     });
 
     //when the user clicks the stop-share-screen button it removes all the screen
