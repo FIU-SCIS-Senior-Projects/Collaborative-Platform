c00394a301e83385de2f493733910aa8a7e34cef
Mandiel Lastra <Mandy@Mandiels-MacBook-Pro.local>
2015-06-12 14:02:37 -0400

    second screen share

--- Code/WebSite/coplat/protected/views/videoConference/join.php
+++ Code/WebSite/coplat/protected/views/videoConference/join.php
@@ -367,9 +367,7 @@
             oneway: true
         });
 
-        rmc.sendCustomMessage({
-            screenToShare: "left"
-        });
+        rmc.sendCustomMessage("left");
     });
 
 
@@ -386,46 +384,44 @@
 //        });
 
 
-//        getScreenId(function (error, sourceId, screen_constraints) {
-//            // error    == null || 'permission-denied' || 'not-installed' || 'installed-disabled' || 'not-chrome'
-//            // sourceId == null || 'string' || 'firefox'
-//
-//            if(sourceId && sourceId != 'firefox') {
-//                screen_constraints = {
-//                    video: {
-//                        mandatory: {
-//                            chromeMediaSource: 'screen',
-//                            maxWidth: 1920,
-//                            maxHeight: 1080,
-//                            minAspectRatio: 1.77
-//                        }
-//                    }
-//                };
-//
-//                if (error === 'permission-denied') return alert('Permission is denied.');
-//                if (error === 'not-chrome') return alert('Please use chrome.');
-//
-//                if (!error && sourceId) {
-//                    screen_constraints.video.mandatory.chromeMediaSource = 'desktop';
-//                    screen_constraints.video.mandatory.chromeMediaSourceId = sourceId;
-//                }
-//            }
-//
-//            navigator.getUserMedia = navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
-//            navigator.getUserMedia(screen_constraints, function (stream) {
-//                document.querySelector('#cotools-panel-2 video').src = URL.createObjectURL(stream);
-//            }, function (error) {
-//                console.error(error);
-//            });
-//        });
+        getScreenId(function (error, sourceId, screen_constraints) {
+            // error    == null || 'permission-denied' || 'not-installed' || 'installed-disabled' || 'not-chrome'
+            // sourceId == null || 'string' || 'firefox'
 
-        rmc.addStream({
-            screen: true,
-            oneway: true
+            if(sourceId && sourceId != 'firefox') {
+                screen_constraints = {
+                    video: {
+                        mandatory: {
+                            chromeMediaSource: 'screen',
+                            maxWidth: 1920,
+                            maxHeight: 1080,
+                            minAspectRatio: 1.77
+                        }
+                    }
+                };
+
+                if (error === 'permission-denied') return alert('Permission is denied.');
+                if (error === 'not-chrome') return alert('Please use chrome.');
+
+                if (!error && sourceId) {
+                    screen_constraints.video.mandatory.chromeMediaSource = 'desktop';
+                    screen_constraints.video.mandatory.chromeMediaSourceId = sourceId;
+                }
+            }
+
+            navigator.getUserMedia = navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+            navigator.getUserMedia(screen_constraints, function (stream) {
+                document.querySelector('#cotools-panel-2 video').src = URL.createObjectURL(stream);
+            }, function (error) {
+                console.error(error);
+            });
         });
-        rmc.sendCustomMessage({
-            screenToShare: "right"
-        });
+
+//        rmc.addStream({
+//            screen: true,
+//            oneway: true
+//        });
+        rmc.sendCustomMessage("right");
 
     });
 
@@ -514,6 +510,7 @@
             console.log("**********************  SCREEN  ********************");
             rmc.onCustomMessage = function(message) {
                 console.log("************************* Messsge received ************************");
+                console.log(message);
             };
 
 //            if(left == 1) {
