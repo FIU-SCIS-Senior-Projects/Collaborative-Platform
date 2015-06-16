770ca22768976fc42dad38a8b291dc25cad5c593
Mandiel Lastra <Mandy@mandiels-mbp.attlocal.net>
2015-06-15 21:16:09 -0400

    change webcams position

--- Code/WebSite/coplat/protected/views/videoConference/join.php
+++ Code/WebSite/coplat/protected/views/videoConference/join.php
@@ -512,7 +512,7 @@
             var uibox = e.mediaElement;
             //document.createElement("div");
             //uibox.appendChild(e.mediaElement);
-            uibox.appendChild(document.createTextNode(e.userid));
+            uibox.appendChild(document.createTextNode("Hellooo"));
             uibox.className = "userid";
             uibox.id = "uibox-" + e.userid.replace(/ |\(|\)/g, '');
 //            uibox.style.cssText = 'display: block';
