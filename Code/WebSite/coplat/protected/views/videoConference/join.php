aa80174a7141a5a58b5097c3b4f21a3c0b434415
Mandiel Lastra <Mandy@mandiels-mbp.attlocal.net>
2015-06-15 22:51:47 -0400

    change webcams position

--- Code/WebSite/coplat/protected/views/videoConference/join.php
+++ Code/WebSite/coplat/protected/views/videoConference/join.php
@@ -169,14 +169,14 @@
 
 
     <div class="row">
-        <div id="cotools-container" class="col-md-6 col-lg-5">
+        <div id="cotools-container" class="col-md-6 col-lg-6">
             <div id="cotools-panel">
 
             </div>
 
         </div>
 
-        <div id="cotools-container-2" class="col-md-6 col-lg-5">
+        <div id="cotools-container-2" class="col-md-6 col-lg-6">
             <div id="cotools-panel-2">
                 <video controls="" autoplay=""></video>
             </div>
