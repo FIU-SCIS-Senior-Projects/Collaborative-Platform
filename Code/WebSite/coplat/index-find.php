ff8237d2dddc8abda1afaf9a9e442b877684229c
Michael Machin <mmach059@fiu.edu>
2015-06-17 15:40:05 -0400

    testing

--- Code/WebSite/coplat/index-find.php
+++ Code/WebSite/coplat/index-find.php
@@ -5,4 +5,31 @@
  * Date: 6/11/2015
  * Time: 1:44 PM
  */
-phpinfo();
+function establishDBConnection()
+{
+    $username = 'root';
+    $password = '9Qst32+';
+    $dbconn = new mysqli("localhost", $username, $password, "coplat");
+    return $dbconn;
+}
+function sendTicketReassignment()
+{
+$dbConnect = establishDBConnection();
+    $query = $dbConnect->query("SELECT * from ticket where id = 87")->fetch_assoc();
+    $ticket_id = $query["id"];
+    $subjectl = $query["subject"];
+    $toEmail = "adurocruor@gmail.com";
+
+    $subject = "Ticket Assigned";
+    $linkAddress = "http://".serverName."index.php/ticket/view/".$ticket_id;
+    $subjectClick = "<a href='$linkAddress'>$subjectl</a>";
+    $body = "Collaborative Platform has assigned you a new ticket:\n\n" . $subjectClick . "\n\nthat was previously assigned to another mentor.\n Thank you for Making Collaborative Platform Great";
+    $headers = 'From: Collaborative Platform <fiucoplat@gmail.com>' . "\r\n" .
+        'Reply-To: fiucoplat@gmail.com' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+//send the email using IMAP
+    if( $a = mail($toEmail, $subject, $body, $headers))
+    {  //  echo "Email sent 3!<br />";
+    }
+}
