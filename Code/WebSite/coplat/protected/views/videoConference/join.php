024ae5dd23702ee2e5e865f3bc8aa35d21d41674
Mandiel Lastra <Mandy@mandiels-mbp.attlocal.net>
2015-06-16 22:59:04 -0400

    screen-share fix

--- Code/WebSite/coplat/protected/views/videoConference/join.php
+++ Code/WebSite/coplat/protected/views/videoConference/join.php
@@ -429,7 +429,8 @@
 
     //when the user clicks the stop-share-screen button it removes all the screen
     $('#stop-share-screen').click(function () {
-        rmc.removeStream('screen');
+        rmc.streams.stop('screen');
+        //rmc.removeStream('screen');
         $('#cotools-panel iframe').show();
         $('#cotools-panel video').remove();
         left = 0;
