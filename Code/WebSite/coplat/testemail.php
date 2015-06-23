891a2b94d0b9929c13bdfd800b3c5380cfd0a4b4
Michael Machin <mmach059@fiu.edu>
2015-06-23 13:57:17 -0700

    starting new implementation to read postfix emails

--- Code/WebSite/coplat/testemail.php
+++ Code/WebSite/coplat/testemail.php
@@ -1,10 +1,4 @@
 <?php
-$to      = 'adurocruor@gmail.com';
-$subject = 'a test email';
-$message = 'hello';
-$headers = 'From: Collaborative Platform <fiucoplat@cp-dev.cs.fiu.edu>' . "\r\n" .
-    'Reply-To: fiucoplat@cp-dev.cs.fiu.edu' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-echo $to;
-mail($to, $subject, $message, $headers);
+$path    = '/home/fiucoplat/Maildir/cur';
+$files = scandir($path);
 ?>
