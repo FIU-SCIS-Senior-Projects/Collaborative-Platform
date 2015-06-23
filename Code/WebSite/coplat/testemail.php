9341bb55ae19453fb2f33acbd61806edee855699
Michael Machin <mmach059@fiu.edu>
2015-06-23 15:29:32 -0700

    see if the commit wors

--- Code/WebSite/coplat/testemail.php
+++ Code/WebSite/coplat/testemail.php
@@ -1,4 +1,14 @@
 <?php
+function establishConnection()
+{
+    $hostname = '{localhost:993/imap/ssl}INBOX';
+    $username = 'fiucoplat@gmail.com';//<script cf-hash="f9e31" type="text/javascript">
+    ///* <![CDATA[ */!function(){try{var t="currentScript"in document?document.currentScript:function(){for(var t=document.getElementsByTagName("script"),e=t.length;e--;)if(t[e].getAttribute("cf-hash"))return t[e]}();if(t&&t.previousSibling){var e,r,n,i,c=t.previousSibling,a=c.getAttribute("data-cfemail");if(a){for(e="",r=parseInt(a.substr(0,2),16),n=2;a.length-n;n+=2)i=parseInt(a.substr(n,2),16)^r,e+=String.fromCharCode(i);e=document.createTextNode(e),c.parentNode.replaceChild(e,c)}}}catch(u){}}();/* ]]> */</script>';
+    $password = 'fiuadmin';
+    $connection = imap_open($hostname, $username, $password);
+    return $connection;
+}
+/*
 $path    = '/home/mmach059/Maildir/new';
 $files = scandir($path);
 foreach ($files as $afile)
