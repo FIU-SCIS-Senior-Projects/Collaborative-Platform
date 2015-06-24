0cde67c5c751c62d5096721faee97a00e621c302
Mandiel Lastra <Mandy@mandiels-mbp.attlocal.net>
2015-06-24 00:21:31 -0400

    second screen-share

--- Code/WebSite/coplat/protected/views/videoConference/join.php
+++ Code/WebSite/coplat/protected/views/videoConference/join.php
@@ -381,69 +381,10 @@
 
     });
 
-//    $('#share-screen-2').click(function () {
-//        sec.open();
-////        sec.connect();
-//        //document.querySelector('#cotools-panel-2 video').src = URL.createObjectURL(stream);
-//
-////        sec.addStream({
-////            screen: true,
-////            oneway: true,
-////            video: true
-////        });
-////
-////
-////        // http://www.rtcmulticonnection.org/docs/addStream/
-////        getScreenId(function (error, sourceId, screen_constraints) {
-////            navigator.getUserMedia = navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
-////            navigator.getUserMedia(screen_constraints, function (sec) {
-////                document.querySelector('#cotools-panel-2 video').src = URL.createObjectURL(sec);
-////            }, function (error) {
-////                console.error(error);
-////            });
-////        });
-//
-//
-////        getScreenId(function (error, sourceId, screen_constraints) {
-////            // error    == null || 'permission-denied' || 'not-installed' || 'installed-disabled' || 'not-chrome'
-////            // sourceId == null || 'string' || 'firefox'
-////
-////            if(sourceId && sourceId != 'firefox') {
-////                screen_constraints = {
-////                    video: {
-////                        mandatory: {
-////                            chromeMediaSource: 'screen',
-////                            maxWidth: 1920,
-////                            maxHeight: 1080,
-////                            minAspectRatio: 1.77
-////                        }
-////                    }
-////                };
-////
-////                if (error === 'permission-denied') return alert('Permission is denied.');
-////                if (error === 'not-chrome') return alert('Please use chrome.');
-////
-////                if (!error && sourceId) {
-////                    screen_constraints.video.mandatory.chromeMediaSource = 'desktop';
-////                    screen_constraints.video.mandatory.chromeMediaSourceId = sourceId;
-////                }
-////            }
-////
-////            navigator.getUserMedia = navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
-////            navigator.getUserMedia(screen_constraints, function (stream) {
-////                document.querySelector('#cotools-panel-2 video').src = URL.createObjectURL(stream);
-////            }, function (error) {
-////                console.error(error);
-////            });
-////        });
-//
-//
-//
-//    });
 
     //when the user clicks the stop-share-screen button it removes all the screen
     $('#stop-share-screen').click(function () {
-        document.getElementById('cotools-panel').setAttribute('has-screen', false);
+        document.getElementById('cotools-panel').setAttribute('has-screen', 0);
         rmc.streams.stop(lStId);
         console.log("===================== REMOVE " + lStId + " ========================");
 //        rmc.streams.stop('screen');
@@ -454,7 +395,7 @@
     });
 
     $('#stop-share-screen-2').click(function () {
-        document.getElementById('cotools-panel-2').setAttribute('has-screen', false);
+        document.getElementById('cotools-panel-2').setAttribute('has-screen', 0);
         rmc.removeStream(rStId);
         console.log("===================== REMOVE " + rStId + " ========================");
 
@@ -534,7 +475,7 @@
         else if (e.isScreen || e.stream.isScreen) {
             if(!document.getElementById('cotools-panel-2').getAttribute('has-screen')) {
                 $('#cotools-panel-2 video').remove();
-                document.getElementById('cotools-panel-2').setAttribute('has-screen', true);
+                document.getElementById('cotools-panel-2').setAttribute('has-screen', 1);
                 document.getElementById('cotools-panel-2').appendChild(e.mediaElement);
                 rStId = e.streamid;
                 console.log("===================== ADD " + rStId + " ========================");
