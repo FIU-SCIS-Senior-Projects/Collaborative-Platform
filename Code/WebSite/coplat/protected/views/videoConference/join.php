c0334f06162d7df3a20c06af6a5c4b5ba00eed85
Mandiel Lastra <Mandy@mandiels-mbp.attlocal.net>
2015-06-23 21:43:38 -0400

    second screen-share

--- Code/WebSite/coplat/protected/views/videoConference/join.php
+++ Code/WebSite/coplat/protected/views/videoConference/join.php
@@ -289,7 +289,7 @@
         document.getElementById("join-room").disabled = true;
         document.getElementById("join-room").innerHTML = 'Waiting for organizer...'
         rmc.connect();
-        sec.connect();
+//        sec.connect();
 //        sec.connect();
         // http://www.rtcmulticonnection.org/docs/connect/
 
@@ -368,11 +368,14 @@
     });
 
     $('#share-screen-2').click(function () {
-        sec.addStream({
-            screen: true,
-            oneway: true,
-            video: true
-        });
+        sec.connect();
+        //document.querySelector('#cotools-panel-2 video').src = URL.createObjectURL(stream);
+
+//        sec.addStream({
+//            screen: true,
+//            oneway: true,
+//            video: true
+//        });
 //
 //
 //        // http://www.rtcmulticonnection.org/docs/addStream/
@@ -386,38 +389,38 @@
 //        });
 
 
-        getScreenId(function (error, sourceId, screen_constraints) {
-            // error    == null || 'permission-denied' || 'not-installed' || 'installed-disabled' || 'not-chrome'
-            // sourceId == null || 'string' || 'firefox'
-
-            if(sourceId && sourceId != 'firefox') {
-                screen_constraints = {
-                    video: {
-                        mandatory: {
-                            chromeMediaSource: 'screen',
-                            maxWidth: 1920,
-                            maxHeight: 1080,
-                            minAspectRatio: 1.77
-                        }
-                    }
-                };
-
-                if (error === 'permission-denied') return alert('Permission is denied.');
-                if (error === 'not-chrome') return alert('Please use chrome.');
-
-                if (!error && sourceId) {
-                    screen_constraints.video.mandatory.chromeMediaSource = 'desktop';
-                    screen_constraints.video.mandatory.chromeMediaSourceId = sourceId;
-                }
-            }
-
-            navigator.getUserMedia = navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
-            navigator.getUserMedia(screen_constraints, function (stream) {
-                document.querySelector('#cotools-panel-2 video').src = URL.createObjectURL(stream);
-            }, function (error) {
-                console.error(error);
-            });
-        });
+//        getScreenId(function (error, sourceId, screen_constraints) {
+//            // error    == null || 'permission-denied' || 'not-installed' || 'installed-disabled' || 'not-chrome'
+//            // sourceId == null || 'string' || 'firefox'
+//
+//            if(sourceId && sourceId != 'firefox') {
+//                screen_constraints = {
+//                    video: {
+//                        mandatory: {
+//                            chromeMediaSource: 'screen',
+//                            maxWidth: 1920,
+//                            maxHeight: 1080,
+//                            minAspectRatio: 1.77
+//                        }
+//                    }
+//                };
+//
+//                if (error === 'permission-denied') return alert('Permission is denied.');
+//                if (error === 'not-chrome') return alert('Please use chrome.');
+//
+//                if (!error && sourceId) {
+//                    screen_constraints.video.mandatory.chromeMediaSource = 'desktop';
+//                    screen_constraints.video.mandatory.chromeMediaSourceId = sourceId;
+//                }
+//            }
+//
+//            navigator.getUserMedia = navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+//            navigator.getUserMedia(screen_constraints, function (stream) {
+//                document.querySelector('#cotools-panel-2 video').src = URL.createObjectURL(stream);
+//            }, function (error) {
+//                console.error(error);
+//            });
+//        });
 
 
 
