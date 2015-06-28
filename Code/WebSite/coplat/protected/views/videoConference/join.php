b3055c0ddbc4cf3e42ea7929887ee01efcfaf9a6
Mandiel Lastra <Mandy@mandiels-mbp.attlocal.net>
2015-06-27 21:05:00 -0400

    add chat

--- Code/WebSite/coplat/protected/views/videoConference/join.php
+++ Code/WebSite/coplat/protected/views/videoConference/join.php
@@ -11,6 +11,14 @@
 
 ?>
 <script>
+    function appendMsg(user, msg) {
+
+        var $cont = $("#chat-history");
+        $cont[0].scrollTop = $cont[0].scrollHeight;
+        $cont.append("<p class='msg'><span>" + user + ":  </span> " + msg + " </p>");
+    }
+</script>
+<script>
     //https://gist.github.com/mathiasbynens/298591
     $.fn.toggleAttr = function (attr, attr1, attr2) {
         return this.each(function () {
@@ -474,12 +482,12 @@
         }
     };
 
-    function appendMsg(user, msg) {
+    // function appendMsg(user, msg) {
 
-        var $cont = $("#chat-history");
-        $cont[0].scrollTop = $cont[0].scrollHeight;
-        $cont.append("<p class='msg'><span>" + user + ":  </span> " + msg + " </p>");
-    }
+    //     var $cont = $("#chat-history");
+    //     $cont[0].scrollTop = $cont[0].scrollHeight;
+    //     $cont.append("<p class='msg'><span>" + user + ":  </span> " + msg + " </p>");
+    // }
 
 
     //removes the div containing the userid of the user who is leaving
