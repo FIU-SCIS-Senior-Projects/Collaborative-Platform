dcb5f011e1683d6626047e964f4b9e442b07911f
Mandiel Lastra <Mandy@Mandiels-MacBook-Pro.local>
2015-07-01 13:10:00 -0400

    sync

--- Code/WebSite/coplat/protected/views/videoConference/join.php
+++ Code/WebSite/coplat/protected/views/videoConference/join.php
@@ -403,7 +403,7 @@
                     $('#cotools-panel iframe').hide();
                     $('#cotools-panel video').remove();
                     document.getElementById('cotools-panel').appendChild(e.mediaElement);
-                }, 2000);
+                }, 200);
 
             }
         }
