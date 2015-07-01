51522d33dee1f9c52fd2943ad4a2841a9716331e
Michael Machin <mmach059@fiu.edu>
2015-07-01 10:27:59 -0700

    adding the ability to view and add by name

--- Code/WebSite/coplat/protected/controllers/AwayMentorController.php
+++ Code/WebSite/coplat/protected/controllers/AwayMentorController.php
@@ -2,122 +2,125 @@
 
 class AwayMentorController extends Controller
 {
-	/**
-	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
-	 * using two-column layout. See 'protected/views/layouts/column2.php'.
-	 */
-	public $layout='//layouts/column2';
+    /**
+     * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+     * using two-column layout. See 'protected/views/layouts/column2.php'.
+     */
+    public $layout='//layouts/column2';
 
-	/**
-	 * @return array action filters
-	 */
-	public function filters()
-	{
-		return array(
-			'accessControl', // perform access control for CRUD operations
-			'postOnly + delete', // we only allow deletion via POST request
-		);
-	}
+    /**
+     * @return array action filters
+     */
+    public function filters()
+    {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+            'postOnly + delete', // we only allow deletion via POST request
+        );
+    }
 
-	/**
-	 * Specifies the access control rules.
-	 * This method is used by the 'accessControl' filter.
-	 * @return array access control rules
-	 */
-	public function accessRules()
-	{
-		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array(),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('remove'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete','remove','index','view','create','update', 'add'),
-				'users'=>array('admin'),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
-		);
-	}
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules()
+    {
+        return array(
+            array('allow',  // allow all users to perform 'index' and 'view' actions
+                'actions'=>array(),
+                'users'=>array('*'),
+            ),
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+                'actions'=>array('remove'),
+                'users'=>array('@'),
+            ),
+            array('allow', // allow admin user to perform 'admin' and 'delete' actions
+                'actions'=>array('admin','delete','remove','index','view','create','update', 'add'),
+                'users'=>array('admin'),
+            ),
+            array('deny',  // deny all users
+                'users'=>array('*'),
+            ),
+        );
+    }
 
-	/**
-	 * Displays a particular model.
-	 * @param integer $id the ID of the model to be displayed
-	 */
-	public function actionView($id)
-	{
-		$this->render('view',array(
-			'model'=>$this->loadModel($id),
-		));
-	}
+    /**
+     * Displays a particular model.
+     * @param integer $id the ID of the model to be displayed
+     */
 
-	/**
-	 * Creates a new model.
-	 * If creation is successful, the browser will be redirected to the 'view' page.
-	 */
-	public function actionCreate()
-	{
-		$model=new AwayMentor;
+    public function actionView($id)
+    {
+        $this->render('view',array(
+            'model'=>$this->loadModel($id),
+        ));
+    }
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+    /**
+     * Creates a new model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     */
+    public function actionCreate()
+    {
+        $model=new AwayMentor;
 
-		if(isset($_POST['AwayMentor']))
-		{
-            $userName = $_POST['AwayMentor']['user_search'];
-            $output = "<script>console.log( 'Debug Objects: " . $userName . "' );</script>";
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
+
+        if(isset($_POST['AwayMentor']))
+        {
+            $userName = $_POST['AwayMentor']['name_search'];
+            $lname = substr($userName, 0, stripos($userName, ","));
+            $fname = substr($userName, stripos($userName, ",")+2);
+            $output = "<script>console.log( 'Debug Objects: " . $lname." ".$fname . "' );</script>";
 
             echo $output;
-            $user = User::model()->findAllBySql("Select * from user where username =:unam", array(":unam"=>$userName));
+            $user = User::model()->findAllBySql("Select * from user where fname =:fnam AND lname =:lnam", array(":fnam"=>$fname, ":lnam"=>$lname));
             foreach($user as $amentor)
             {
                 $output = "<script>console.log( 'Debug Objects: " . $amentor->id . "' );</script>";
                 echo $output;
                 $model->userID=$amentor->id;
                 if($model->save())
-                    $this->redirect(array('view','id'=>$model->userID));
+                    $this->redirect(array('admin'));
             }
 
 
-		}
+        }
 
-		$this->render('create',array(
-			'model'=>$model,
-		));
-	}
+        $this->render('create',array(
+            'model'=>$model,
+        ));
+    }
 
-	/**
-	 * Updates a particular model.
-	 * If update is successful, the browser will be redirected to the 'view' page.
-	 * @param integer $id the ID of the model to be updated
-	 */
+    /**
+     * Updates a particular model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id the ID of the model to be updated
+     */
 
     public function actionUpdate($id)
-	{
-		$model=$this->loadModel($id);
+    {
+        $model=$this->loadModel($id);
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
 
-		if(isset($_POST['AwayMentor']))
-		{
-			$model->attributes=$_POST['AwayMentor'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->userID));
-		}
+        if(isset($_POST['AwayMentor']))
+        {
+            $model->attributes=$_POST['AwayMentor'];
+            if($model->save())
+                $this->redirect(array('view','id'=>$model->userID));
+        }
 
-		$this->render('update',array(
-			'model'=>$model,
-		));
-	}
+        $this->render('update',array(
+            'model'=>$model,
+        ));
+    }
     public function actionRemove($id)
     {
-    AwayMentor::model()->deleteByPk($id);
+        AwayMentor::model()->deleteByPk($id);
         Yii::app()->request->redirect(Yii::app()->homeURL);
     }
     public function actionAdd($id)
@@ -128,71 +131,71 @@ class AwayMentorController extends Controller
         $model->save();
         $this->redirect('/coplat/index.php/AwayMentor/admin');
     }
-	/**
-	 * Deletes a particular model.
-	 * If deletion is successful, the browser will be redirected to the 'admin' page.
-	 * @param integer $id the ID of the model to be deleted
-	 */
-	public function actionDelete($id)
-	{
-		$this->loadModel($id)->delete();
+    /**
+     * Deletes a particular model.
+     * If deletion is successful, the browser will be redirected to the 'admin' page.
+     * @param integer $id the ID of the model to be deleted
+     */
+    public function actionDelete($id)
+    {
+        $this->loadModel($id)->delete();
 
-		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-		if(!isset($_GET['ajax']))
-			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-	}
+        // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+        if(!isset($_GET['ajax']))
+            $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+    }
 
-	/**
-	 * Lists all models.
-	 */
-	public function actionIndex()
-	{
-		$dataProvider=new CActiveDataProvider('AwayMentor');
-		$this->render('index',array(
-			'dataProvider'=>$dataProvider,
-		));
-	}
+    /**
+     * Lists all models.
+     */
+    public function actionIndex()
+    {
+        $dataProvider=new CActiveDataProvider('AwayMentor');
+        $this->render('index',array(
+            'dataProvider'=>$dataProvider,
+        ));
+    }
 
-	/**
-	 * Manages all models.
-	 */
-	public function actionAdmin()
-	{
-		$model=new AwayMentor('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['AwayMentor']))
-			$model->attributes=$_GET['AwayMentor'];
+    /**
+     * Manages all models.
+     */
+    public function actionAdmin()
+    {
+        $model=new AwayMentor('search');
+        $model->unsetAttributes();  // clear any default values
+        if(isset($_GET['AwayMentor']))
+            $model->attributes=$_GET['AwayMentor'];
 
-		$this->render('admin',array(
-			'model'=>$model,
-		));
-	}
+        $this->render('admin',array(
+            'model'=>$model,
+        ));
+    }
 
-	/**
-	 * Returns the data model based on the primary key given in the GET variable.
-	 * If the data model is not found, an HTTP exception will be raised.
-	 * @param integer $id the ID of the model to be loaded
-	 * @return AwayMentor the loaded model
-	 * @throws CHttpException
-	 */
-	public function loadModel($id)
-	{
-		$model=AwayMentor::model()->findByPk($id);
-		if($model===null)
-			throw new CHttpException(404,'The requested page does not exist.');
-		return $model;
-	}
+    /**
+     * Returns the data model based on the primary key given in the GET variable.
+     * If the data model is not found, an HTTP exception will be raised.
+     * @param integer $id the ID of the model to be loaded
+     * @return AwayMentor the loaded model
+     * @throws CHttpException
+     */
+    public function loadModel($id)
+    {
+        $model=AwayMentor::model()->findByPk($id);
+        if($model===null)
+            throw new CHttpException(404,'The requested page does not exist.');
+        return $model;
+    }
 
-	/**
-	 * Performs the AJAX validation.
-	 * @param AwayMentor $model the model to be validated
-	 */
-	protected function performAjaxValidation($model)
-	{
-		if(isset($_POST['ajax']) && $_POST['ajax']==='away-mentor-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
-	}
+    /**
+     * Performs the AJAX validation.
+     * @param AwayMentor $model the model to be validated
+     */
+    protected function performAjaxValidation($model)
+    {
+        if(isset($_POST['ajax']) && $_POST['ajax']==='away-mentor-form')
+        {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
+    }
 }
--- Code/WebSite/coplat/protected/views/awayMentor/admin.php
+++ Code/WebSite/coplat/protected/views/awayMentor/admin.php
@@ -3,12 +3,12 @@
 /* @var $model AwayMentor */
 
 $this->breadcrumbs=array(
-	'Away Mentors'=>array('index'),
-	'Manage',
+    'Away Mentors'=>array('index'),
+    'Manage',
 );
 
 $this->menu=array(
-	array('label'=>'List AwayMentor', 'url'=>array('index')),
+    array('label'=>'List AwayMentor', 'url'=>array('index')),
     array('label'=>'Create AwayMentor', 'url'=>array('create')),
 );
 
@@ -29,28 +29,29 @@ $('.search-form form').submit(function(){
 <h1>Manage Away Mentors</h1>
 
 <p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
+    You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
+    or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
 </p>
 
 <?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
 <div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
+    <?php $this->renderPartial('_search',array(
+        'model'=>$model,
+    )); ?>
 </div><!-- search-form -->
 
 <?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'away-mentor-grid',
-	'dataProvider'=>$model->search(),
-	'filter'=>$model,
-	'columns'=>array(
-		'userID',
+    'id'=>'away-mentor-grid',
+    'dataProvider'=>$model->search(),
+    'filter'=>$model,
+    'columns'=>array(
+        'userID',
         array('name'=>'user_search','value'=>'$data->user->username'),
-		'tiStamp',
-       	array(
-			'class'=>'CButtonColumn',
+        array('name'=>'name_search','value'=>'$data->user->getFullName()'),
+        'tiStamp',
+        array(
+            'class'=>'CButtonColumn',
             'template'=>'{view}{delete}',
-		),
-	),
+        ),
+    ),
 )); ?>
