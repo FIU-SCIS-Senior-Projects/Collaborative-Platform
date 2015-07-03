aff29ce5ecd6799e249b479e456756331684421d
Mandiel Lastra <Mandy@mandiels-mbp.attlocal.net>
2015-07-03 18:17:56 -0400

    chat counter

--- Code/WebSite/coplat/protected/views/videoConference/join.php
+++ Code/WebSite/coplat/protected/views/videoConference/join.php
@@ -460,7 +460,7 @@
             appendMsg(username, event.data.content);
             messages++;
             //if(!$('#count').hasClass('mm-opened')) {
-                $('#count').text = messages;
+                $('#count').text = 2;
                 $('.chat-message-counter').show();
             //}
         }
