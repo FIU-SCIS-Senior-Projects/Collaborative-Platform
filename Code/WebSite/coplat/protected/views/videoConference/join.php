bef03bb0cdd65d52cdb10aa8791f119a970de9ea
Mandiel Lastra <Mandy@mandiels-mbp.attlocal.net>
2015-07-05 20:27:31 -0400

    no webcam

--- Code/WebSite/coplat/protected/views/videoConference/join.php
+++ Code/WebSite/coplat/protected/views/videoConference/join.php
@@ -389,8 +389,8 @@
             document.getElementById("on-off-video").style.color= 'red';
             $('#join-room').fadeOut(600);
             if(!rmc.DetectRTC.hasWebcam) {
-                alert("\n" + e.userid.replace(/ |\(|\)/g, ''));
-<!--                --><?php //echo $user->fname . ' ' .$user->lname .' ('. $user->username .')'?>
+                alert("-" + <?php echo $user->fname.$user->lname.$user->username ?> + "-\n-" + e.userid.replace(/ |\(|\)/g, '') + "-");
+
                 $('#uibox-' + <?php echo $user->id ?>).prepend('<img id="nwcImage" src="/coplat/images/noWebcam.png" />');
             }
 //            alert(e.streamid);
