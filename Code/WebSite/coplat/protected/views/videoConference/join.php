3115e46a8a43bcaaad3d1b102563f718452cccb6
Mandiel Lastra <Mandy@mandiels-mbp.attlocal.net>
2015-07-06 23:24:42 -0400

    working backup

--- Code/WebSite/coplat/protected/views/videoConference/join.php
+++ Code/WebSite/coplat/protected/views/videoConference/join.php
@@ -374,28 +374,20 @@
             // alert("the stream is remote");
         }
         if (e.isVideo || e.stream.isVideo) {
-
-                var uibox = document.createElement("div");
-                uibox.appendChild(document.createTextNode(e.userid));
-                uibox.appendChild(e.mediaElement);
-                uibox.className = "userid";
-                uibox.id = "uibox-" + e.userid.replace(/ |\(|\)/g, '');
-                uibox.style.cssText = 'float: left';
-                e.mediaElement.style.cssText = 'display: block';
-                document.getElementById('video-container').appendChild(uibox);
-                document.getElementById("on-off-video").style.color = 'red';
+            var uibox = document.createElement("div");
+            uibox.appendChild(document.createTextNode(e.userid));
+            uibox.appendChild(e.mediaElement);
+            uibox.className = "userid";
+            uibox.id = "uibox-" + e.userid.replace(/ |\(|\)/g, '');
+            uibox.style.cssText = 'float: left';
+            e.mediaElement.style.cssText = 'display: block';
+            document.getElementById('video-container').appendChild(uibox);
 
             if(!rmc.DetectRTC.hasWebcam) {
                 document.getElementById("on-off-video").remove();
-                var nwVid = document.createElement("video");
-                nwVid.poster = "/coplat/images/noWebcam.png";
-                nwVid.autoplay = true;
-                document.getElementById('video-container').appendChild(nwVid);
-
             }
-
+            document.getElementById("on-off-video").style.color= 'red';
             $('#join-room').fadeOut(600);
-
         }
         else if (e.isAudio) {
             document.getElementById('video-container').appendChild(e.mediaElement);
