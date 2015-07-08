c5d8b0c021c84e67801a51d7961cacf4177fb440
Michael Machin <mmach059@fiu.edu>
2015-07-08 12:06:36 -0700

    Order by

--- Code/WebSite/coplat/protected/controllers/ProjectMeetingController.php
+++ Code/WebSite/coplat/protected/controllers/ProjectMeetingController.php
@@ -306,7 +306,7 @@ class ProjectMeetingController extends Controller
         /*Get all tickets for his mentees */
         $tickets = array();
         foreach($pmentee as  $id=>$menteeTickets){
-            $myTickets = Ticket::model()->findAllBySql("SELECT * FROM ticket WHERE creator_user_id=:id and assign_user_id!=:id2",
+            $myTickets = Ticket::model()->findAllBySql("SELECT * FROM ticket WHERE creator_user_id=:id and assign_user_id!=:id2 order by id desc",
                 array(':id'=>$menteeTickets->id, ':id2'=>User::getCurrentUserId()));
             if(is_array($myTickets)) {
                 $tickets = array_merge($tickets, $myTickets);
