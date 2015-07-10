387837d25e3ecfec9ca0e625a9c47d729a5a61a6
mlast004 <mlast004@fiu.edu>
2015-07-10 10:22:09 -0400

    Update _form.php

--- Code/WebSite/coplat/protected/views/awayMentor/_form.php
+++ Code/WebSite/coplat/protected/views/awayMentor/_form.php
@@ -77,3 +77,10 @@
     </body>
     </html>
 </div><!-- form -->
+
+<?php if (Yii::app()->user->hasFlash('invitation-error')): ?>
+    <h3>There was an error: </h3>
+    <div style="margin-top:20px;" class="error-message">
+        <?php echo Yii::app()->user->getFlash('invitation-error'); ?>
+    </div>
+<?php endif; ?>
