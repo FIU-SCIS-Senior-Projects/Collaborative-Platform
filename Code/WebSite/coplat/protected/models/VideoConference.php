fb4b7f05e4109e0866291e4cbbb36a8492a29b98
Mandiel Lastra <Mandy@Mandiels-MacBook-Pro.local>
2015-07-14 21:38:40 -0400

    view deleted conferences

--- Code/WebSite/coplat/protected/models/VideoConference.php
+++ Code/WebSite/coplat/protected/models/VideoConference.php
@@ -16,101 +16,93 @@
  */
 class VideoConference extends CActiveRecord
 {
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @param string $className active record class name.
-	 * @return VideoConference the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+    public $dateToString;
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className active record class name.
+     * @return VideoConference the static model class
+     */
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'video_conference';
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'video_conference';
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-//			array('id, moderator_id', 'required'),
-//			array('id, moderator_id', 'length', 'max'=>11),
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+//          array('id, moderator_id', 'required'),
+//          array('id, moderator_id', 'length', 'max'=>11),
             array('subject', 'required'),
             array('subject', 'length', 'max'=>255),
-			array('notes', 'length', 'max'=>255),
-			array('scheduled_on, scheduled_for', 'safe'),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('id, subject, moderator_id, scheduled_on, scheduled_for, notes', 'safe', 'on'=>'search'),
-		);
-	}
+            array('notes', 'length', 'max'=>255),
+            array('scheduled_on, scheduled_for', 'safe'),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('id, subject, moderator_id, scheduled_on, scheduled_for, notes', 'safe', 'on'=>'search'),
+        );
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			'moderator' => array(self::BELONGS_TO, 'User', 'moderator_id'),
-		);
-	}
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'moderator' => array(self::BELONGS_TO, 'User', 'moderator_id'),
+            'invitations'=>array(self::HAS_MANY, 'VCInvitation', 'videoconference_id'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id' => 'ID',
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'id' => 'ID',
             'subject' => 'Subject',
-			'moderator_id' => 'Moderator',
-			'scheduled_on' => 'Scheduled On',
-			'scheduled_for' => 'Date',
-			'notes' => 'Notes',
-		);
-	}
+            'moderator_id' => 'Moderator',
+            'scheduled_on' => 'Scheduled On',
+            'scheduled_for' => 'Date',
+            'notes' => 'Notes',
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
-
-		$criteria=new CDbCriteria;
-
-		$criteria->compare('id',$this->id,true);
-        $criteria->compare('subject',$this->subject,true);
-		$criteria->compare('moderator_id',$this->moderator_id,true);
-		$criteria->compare('scheduled_on',$this->scheduled_on,true);
-		$criteria->compare('scheduled_for',$this->scheduled_for,true);
-		$criteria->compare('notes',$this->notes,true);
-
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
-
-    public function searchDeleted()
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search()
     {
         // Warning: Please modify the following code to remove attributes that
         // should not be searched.
 
+        $criteria=new CDbCriteria;
+
+        $criteria->compare('id',$this->id,true);
+        $criteria->compare('subject',$this->subject,true);
+        $criteria->compare('moderator_id',$this->moderator_id,true);
+        $criteria->compare('scheduled_on',$this->scheduled_on,true);
+        $criteria->compare('scheduled_for',$this->scheduled_for,true);
+        $criteria->compare('notes',$this->notes,true);
+
         return new CActiveDataProvider($this, array(
-            'criteria'=>array('condition'=>'status like "deleted"')
+            'criteria'=>$criteria,
         ));
     }
 
@@ -135,7 +127,30 @@ class VideoConference extends CActiveRecord
 */
     }
 
+    public function searchDeleted()
+    {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
 
+        return new CActiveDataProvider($this, array(
+            'criteria'=>array('condition'=>'status like "deleted"')
+        ));
+    }
+
+    public function searchUpcoming($id)
+    {
+        return new CActiveDataProvider($this, array(
+            'criteria'=>array(
+                'condition'=>'(moderator_id ='.$id.' or x.invitee_id = '.$id.') and scheduled_for >= DATE_ADD(NOW() , INTERVAL -1 HOUR )',
+                'join'=> 'left join (select * from vc_invitation where invitee_id = '.$id.')x on t.id = x.videoconference_id'
+            ),
+                'sort'=>array(
+                'defaultOrder'=>'scheduled_for ASC',
+
+            ),
+        ));
+
+    }
     public function findParticipantsAsString(){
         $moderator = User::model()->findByAttributes(array("id" => $this->moderator_id));
         $str = $moderator->fname . " " .$moderator->lname;
@@ -210,7 +225,24 @@ class VideoConference extends CActiveRecord
         return $str;
     }
 
+    public function getDateToString()
+    {
+        $date = new DateTime($this->scheduled_for);
+        $now = new DateTime("now");
+        if ($date->format('Y-m-d') == date('Y-m-d'))
+        {
+           // if($now > $date) {
+            //    return "Today, " . date(" g:i A", strtotime($this->scheduled_for)) . " LATE";
+           // }
+           // else {
+                return "Today, " . date(" g:i A", strtotime($this->scheduled_for));
+           // }
+        }
+        else {
+            return date("M d, g:i A", strtotime($this->scheduled_for));
+        }
 
+    }
     public  function cancel(){
         $this->status = "cancelled";
 
@@ -222,11 +254,6 @@ class VideoConference extends CActiveRecord
         }
 
 
-        return $this->save();
-    }
-
-    public  function delete(){
-        $this->status = "deleted";
         return $this->save();
     }
 }
